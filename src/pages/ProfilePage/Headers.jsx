5624784502df7cb24f53891919e8f7dd2630125e
Dhvani19 <dhvani.cce21@sot.pdpu.ac.in>
2025-04-25 12:34:02 +0530

    updated

--- src/pages/ProfilePage/Headers.jsx
+++ src/pages/ProfilePage/Headers.jsx
@@ -1,14 +1,15 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import prllogo from '../../assets/prl-logo.png';
 import prlogo from '../../assets/prime.png';
-import { LogIn, LogOut } from 'lucide-react'
+import { LogIn, LogOut } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-const Headers = () => {
 
-const navigate = useNavigate();
- const [isLoggedIn, setIsLoggedIn] = useState(false);
- useEffect(() => {
+const Headers = () => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
     const checkLoginStatus = () => {
       const authToken = Cookies.get('authToken');
       setIsLoggedIn(!!authToken);
@@ -19,35 +20,36 @@ const navigate = useNavigate();
     return () => clearInterval(intervalId);
   }, []);
 
-const handleHomeClick=()=>{
-  navigate('/');
-};
-const handleLoginClick=()=>{
-  navigate("/login");
-};
+  const handleHomeClick = () => {
+    navigate('/');
+  };
 
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+  
   return (
     <div className="flex justify-between items-center px-12 py-2">
-     
-      <div className="flex space-x-4 items-center">
-        <div className="Logo ">
+      <div className="flex items-center justify-center space-x-6 flex-1">
+        <div className="Logo flex justify-center">
           <img onClick={handleHomeClick} src={prllogo} alt="PRL Logo" />
         </div>
-        <div className="Mainlogo ">
+        <div className="Mainlogo flex justify-center">
           <img onClick={handleHomeClick} src={prlogo} alt="Main Logo" />
         </div>
       </div>
-
+      
       <div className="flex space-x-4">
-        <button 
-        onClick={handleHomeClick}
-        className="px-4 py-2 text-gray-700 hover:text-gray-900 font-semibold rounded-md hover:bg-gray-100">
+        <button
+          onClick={handleHomeClick}
+          className="px-4 py-2 text-gray-700 hover:text-gray-900 font-semibold rounded-md hover:bg-gray-100"
+        >
           Home
         </button>
-        <button 
-        onClick={handleLoginClick}
-        icon={<LogIn />}
-        className="px-7 py-3 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600 flex items-center gap-2">
+        <button
+          onClick={handleLoginClick}
+          className="px-7 py-3 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600 flex items-center gap-2"
+        >
           <span>{isLoggedIn ? 'Logout' : 'Login'}</span>
           {isLoggedIn ? <LogOut size={20} /> : <LogIn size={20} />}
         </button>
