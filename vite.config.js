d998e61dc73e78293b7e31a010014e96978f06cd
alamprl <alamprl@gmail.com>
2025-11-06 15:37:49 +0530

    ssl-implementation-6nov2025

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    allowedHosts: ['prime.lan.prl.res.in'],
+   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
